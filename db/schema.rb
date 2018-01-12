9329579166eb1749dc3276d89d7a341283d90102
marizaswart <mariza.swart@gmail.com>
2018-01-12 20:59:03 +0100

    Add unit tests for menu

--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,4 @@ ActiveRecord::Schema.define(version: 20180112194712) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "restaurants", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :name }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name }
+  end
+
+  describe FactoryBot do
+    it 'should be valid' do
+      expect(FactoryBot.create(:menu)).to be_valid
+    end
+  end
 end
