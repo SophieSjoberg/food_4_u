4bd49e5c114f880bc73d4aa4e9d0a2b7ef33d1a1
marizaswart <mariza.swart@gmail.com>
2018-01-13 12:42:00 +0100

    Create the customer and charge for stripe transaction

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,20 @@
 class CartController < ApplicationController
   def show
   end
+
+  def create
+    @amount = 1000
+    customer = Stripe::Customer.create(
+      email: params[:stripeEmail],
+      source: params[:stripeToken]
+    )
+    
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: 'payment for food',
+      currency: 'sek'
+    )
+
+  end
 end
