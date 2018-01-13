7ebbec3607bda1798e2d2a3ff24ce6bf907ad470
ScottB <17sb@cis.dk>
2018-01-13 16:25:23 +0100

    Refactor files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,14 +15,12 @@
 //= require turbolinks
 //= require_tree .
 $(document).ready(function() {
-
   var scrollLink = $('.scroll');
 
-  // Smooth scrolling
-  scrollLink.click(function(e) {
-    e.preventDefault();
+  scrollLink.click(function(event) {
+    event.preventDefault();
     $('body,html').animate({
       scrollTop: $(this.hash).offset().top
-    }, 1200);
+    }, 1500);
   });
 });
