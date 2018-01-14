9940af2ce4396330c45abf50f445a49e09135e87
ScottB <17sb@cis.dk>
2018-01-14 21:17:15 +0100

    Refactor seeds file with while loop

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,20 +15,11 @@ end
 seed_file = Rails.root.join('db', 'seeds', 'menus.yml')
 config = YAML::load_file(seed_file)
 counter = 0
-Restaurant.all[counter].menus.create!(config) do |c|
-  puts "Create menu #{counter += 1} with name: #{c.name}"
-end
+counter2 = 0
 
-seed_file = Rails.root.join('db', 'seeds', 'menus.yml')
-config = YAML::load_file(seed_file)
-counter = 1
-Restaurant.all[counter].menus.create!(config) do |c|
-  puts "Create menu #{counter += 1} with name: #{c.name}"
-end
-
-seed_file = Rails.root.join('db', 'seeds', 'menus.yml')
-config = YAML::load_file(seed_file)
-counter = 2
-Restaurant.all[counter].menus.create!(config) do |c|
-  puts "Create menu #{counter += 1} with name: #{c.name}"
+while counter2 < Restaurant.all.count
+  Restaurant.all[counter2].menus.create!(config) do |c|
+    puts "Create menu #{counter += 1} with name: #{c.name}"
+  end
+  counter2 += 1
 end
