80b591902e896111aa43921d5e961ac11baa3d31
ScottB <17sb@cis.dk>
2018-01-14 21:43:44 +0100

    Refactor seeds file (hopefully last time)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,8 @@ end
 
 seed_file = Rails.root.join('db', 'seeds', 'menus.yml')
 config = YAML::load_file(seed_file)
-counter = 0
-while counter < Restaurant.all.count
-  Restaurant.all[counter].menus.create!(config) do |c|
+Restaurant.all.each do |restaurant|
+  restaurant.menus.create!(config) do |c|
     puts "Create menu with name: #{c.name}"
   end
-  counter += 1
 end
